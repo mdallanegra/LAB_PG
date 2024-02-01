3b304d9c2cdccefabaf975147f12497c6bf6d5f5
Miguel Angel Dallanegra Vilches <mdallanegra@icloud.com>
2024-02-01 13:01:43 -0300

    commit

--- HotReviews/HotReviews/settings.py
+++ HotReviews/HotReviews/settings.py
@@ -124,11 +124,13 @@ STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'home/static'),
+]
 SITE_ID = 1
 
 FAVICON_URL = 'favicon.ico'
-FAVICON_ROOT = BASE_DIR / 'static/favicon.ico'
+FAVICON_ROOT = BASE_DIR / 'static/imagenes/favicon.ico'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- HotReviews/home/urls.py
+++ HotReviews/home/urls.py
@@ -1,6 +1,9 @@
+from django.views.generic.base import RedirectView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('favicon.ico', RedirectView.as_view(
+        url='/static/imagenes/favicon.ico')),
 ]
